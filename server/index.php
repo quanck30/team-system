fec9328d1a46ce4e726ed507a86883e70184eaa6
kairi <2250159@ecc.ac.jp>
2026-04-21 15:36:24 +0900

    ログイン画面（サーバー側）

--- server/index.php
+++ server/index.php
@@ -1,11 +1,24 @@
 <?php
+//ログイン画面のサーバー側
+//マサキカイリ
 // Home.phpからもらってきたログイン情報をここで処理してログイン情報を返す
-if($_SERVER["REQUEST_METHOD"] === "POST"){
 
+//POSTじゃないならHomeに返す
+if($_SERVER["REQUEST_METHOD"] !== "POST"){
+header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
+exit;
 }
+
 require_once __DIR__ . "../helpers/def.php";
 require_once __DIR__ . "../helpers/utils.php";
 
+function modoru($dept_no,$page){//管理者なのかチェック
+    if ($dept_no === "1") {
+        header("Location: " . TEAM_SYSTEM . "/client/page/" . $page . ".php");
+    } else {
+        header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
+    }
+}
 //セッションスタート
 session_start();
 //セッションにデータを保存
@@ -15,26 +28,27 @@ $_SESSION['message'];
 $emp_no = $_POST['emp_no'];
 if(isset($emp_no)){
     $_SESSION['message'] = "従業員番号が空です。";
-    header('Location: ../client/page/Home.php');
+    header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
     exit;
 }
 //　IDがint型か
 if(!is_int($emp_no)){
     $_SESSION['message'] = "従業員番号に数字以外が入っています";
-    header('Location: ../client/page/Home.php');
+    header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
     exit;
 }
+
 
 //　パスワードは空じゃないか
 $pass = $_POST['password'];
 if(isset($pass)){
     $_SESSION['message'] = "パスワードが空です。";
-    header('Location: ../client/page/Home.php');
+    header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
     exit;
 }
 
 // パスワードをハッシュ化
-$hashpass = password_hash($pass , PASSWORD_DEFAULT);
+// $hashpass = password_verify($pass , PASSWORD_DEFAULT);
 
 
 try{
@@ -48,30 +62,34 @@ try{
     //bindValueで型が正しいか確認
     $stmt->bindValue(':emp_no' , $emp_no , PDO::PARAM_INT);
 
-    // ハッシュ化したパスワードを照合
-    $password = $stmt['password'];
-    if($password === $hashpass){
+    //resultに結果を格納
+     $stmt->execute();
+     $user = $stmt -> fetch();
 
-        /* システム管理部にチェックがついてたら管理者用メニューに遷移
-        チェックがついていなかったら社員メニューに遷移
-        */
+    // ハッシュ化したパスワードを照合
+    $password = $user['password'];
+    if(password_verify( $pass , $user['password'])){
+        //dept_no(部署)が１なら管理人の画面に遷移
+        //headerの管理人側のページを変数に格納
+        $page = "TODO";
+        $dept_no = $user['DEPT_NO'];
+
+        // セッションの保存（社員番号）
+        $_SESSION['emp_no'] = $emp_no;
+        modoru($dept_no,$page);
         
-        if($stmt['dept_no'] === "1"){
-            header('Location:../client/page/TODO.php');
-        } else {
-            header('Location: ../client/page/Home.php');
-        }
+        exit;
         
     //パスワードが間違ってたらHomeに遷移
     } else {
-        header('Location: ../client/page/Home.php');
+        header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
         exit;
     }
 
-    //TODO:セッションの保存
-
 } catch (PDOException $poe) {
-    exit("DBエラー" . $poe->getMessage());//開発時だけメッセージ表示
+    header("Location: " . TEAM_SYSTEM . "/client/page/Home.php");
+    exit;
+    // exit("DBエラー" . $poe->getMessage());//開発時だけメッセージ表示
 }
 
 ?>
