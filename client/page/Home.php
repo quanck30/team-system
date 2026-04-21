c1275225560f0e53a3fd26bff390015d0dc32318
imasakimimaru-commits <imasakimimaru@gmail.com>
2026-04-21 16:35:32 +0900

    CSSの適用

--- client/page/Home.php
+++ client/page/Home.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
+    <link rel="stylesheet" href="dezain.css">
 </head>
 <body>
-    <h1>ログインホーム</h1>
+    
+    <header>
+       <h1>ログインホーム</h1>
+    </header>
+
+    <!-- ログインフォーム -->
+
+
+    <div class="box">
+        <div class="title">
+           
     <a href="touroku.php">安全登録画面</a><br>
     <a href="manager.php">管理者用</a>
+    </div>
+        </div>
 
 </body>
 </html>
--- client/page/safetyList.php
+++ client/page/safetyList.php
@@ -4,18 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>社員安否一覧画面</title>
+    <link rel="stylesheet" href="dezain.css">
+
+
 </head>
 <body>
-    <h1>社員安否一覧画面</h1>
-    <a href="safetydetail.php">安否詳細画面</a>
-
+    <header>
+        <h1>社員安否一覧画面</h1>
+    </header>
+    
+    
+    <div class="box">
+        <div class=title>
     <h2>一覧</h2>
+        </div>
 
     <?php $emp_no = $_POST['safety'] ?>
 
 
     
 
-    <?php echo $emp_no  ?>
+    <?php echo $emp_no  ?><br>
+    </div>
+    
+        <div class="title">
+    <a href="safetydetail.php">安否詳細画面</a>
+        
+    </div>
 </body>
 </html>
