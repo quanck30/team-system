d2491b92590d61a9a4a5b6c3616846e53543069a
imasakimimaru-commits <imasakimimaru@gmail.com>
2026-04-24 15:30:14 +0900

    進捗

--- client/page/safetyList.php
+++ client/page/safetyList.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>社員安否一覧画面</title>
-    <link rel="stylesheet" href="dezain.css">
+    <link rel="stylesheet" href="../css/dezain.css">
+
 
 
 </head>
