949f94c8b0c8aa7d1eff4d5677b664bf7fc9c02a
Phyo Zaw Aung <phyozawaung673@gmail.com>
2026-04-24 16:47:04 +0900

    design

--- client/index.php
+++ client/index.php
@@ -15,7 +15,7 @@ unset($_SESSION["pass_err"]);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
