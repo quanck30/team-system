0046cb552b5d3cb6361ed24c2a8d7c2051c16978
quanck30 <quanck30@gmail.com>
2026-04-24 14:22:40 +0900

    ログイン機能追加

--- helpers/def.php
+++ helpers/def.php
@@ -4,7 +4,7 @@
  * 定数を定義する
  */
 
-define("TEAM_SYSTEM", "http://localhost/team_system");
+define("TEAM_SYSTEM", "http://localhost/team-system");
 
 define("ASSET_URL", TEAM_SYSTEM . "/asset");
 
@@ -16,4 +16,3 @@ define("DB_USER", "user");
 define("DB_PASS", "password");
 define("DB_NAME", "dbname");
 define("DB_CHARSET", "utf8mb4");
-
