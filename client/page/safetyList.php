a2f8fbea9ac40c9b789a764ac4a7860efb002ba4
imasakimimaru-commits <imasakimimaru@gmail.com>
2026-05-08 15:53:08 +0900

    commit

--- client/page/safetyList.php
+++ client/page/safetyList.php
@@ -20,6 +20,8 @@ $all_safety = get_all_safety();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>社員安否一覧画面</title>
     <link rel="stylesheet" href="./../css/safetyList.css">
+    <!-- スマホ対応 -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 
 <body>
